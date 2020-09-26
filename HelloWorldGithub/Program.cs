b9e0058fd00ae86ad3407bd5c7cf6f202e89ef0c
Bernard Gabon <hewbertgabon@gmail.com>
2020-09-26 23:44:55 +0800

    edited in sharpdevelop

--- HelloWorldGithub/Program.cs
+++ HelloWorldGithub/Program.cs
@@ -9,6 +9,9 @@ namespace HelloWorldGithub
 		{
 			Console.WriteLine("Hello World and Hello Github!");			
 			Console.WriteLine("I have edited this code within Github.com");	
+			
+			Console.WriteLine("I have edited this code in Shap develop");	
+			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
